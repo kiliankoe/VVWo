45637e1cba02d364775db9ce4434101bcdddec93
Kilian Koeltzsch <me@kilian.io>
2019-11-09 16:24:47 +0100

    Initial Commit

---
+++ VVWo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VVWo
+//
+//  Created by Kilian Koeltzsch on 09.11.19.
+//  Copyright © 2019 Kilian Koeltzsch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
